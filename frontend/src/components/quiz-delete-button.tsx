bb682cd62710d42e3b609f32fe755748739449ab
wagao <wagao1029@gmail.com>
2024-09-08 15:07:32 +0900

    Add alert dialog to quiz delete button

--- frontend/src/components/quiz-delete-button.tsx
+++ frontend/src/components/quiz-delete-button.tsx
@@ -1,8 +1,18 @@
 "use client";
 
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
 import { Trash2 } from "lucide-react";
-
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { toastError, toastSuccess } from "./toasts";
 import { Button } from "./ui/button";
 
@@ -12,24 +22,60 @@ type Props = React.ComponentProps<typeof Button> & {
 
 export function QuizDeleteButton({ quizId, className, ...props }: Props) {
   const router = useRouter();
+  const [openDialog, setOpenDialog] = useState(false);
+
+  const onClickDelete = async () => {
+    const response = await fetch(`/api/quizzes/${quizId}`, {
+      method: "DELETE",
+    });
+    if (response.ok) {
+      router.refresh();
+      toastSuccess("クイズを削除しました");
+    } else {
+      toastError("クイズの削除に失敗しました");
+    }
+    setOpenDialog(false);
+  };
+
   return (
-    <Button
-      size="icon"
-      variant="ghost"
-      onClick={async () => {
-        const response = await fetch(`/api/quizzes/${quizId}`, {
-          method: "DELETE",
-        });
-        if (response.ok) {
-          router.refresh();
-          toastSuccess("クイズを削除しました");
-        } else {
-          toastError("クイズの削除に失敗しました");
-        }
-      }}
-      {...props}
-    >
-      <Trash2 color="red" />
-    </Button>
+    <>
+      <Button
+        size="icon"
+        variant="ghost"
+        onClick={() => {
+          setOpenDialog(true);
+        }}
+        {...props}
+      >
+        <Trash2 color="red" />
+      </Button>
+      <AlertDialog open={openDialog}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>
+              本当にこのクイズを削除しますか？
+            </AlertDialogTitle>
+            <AlertDialogDescription>
+              この操作は取り消せません。
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel
+              onClick={() => {
+                setOpenDialog(false);
+              }}
+            >
+              キャンセル
+            </AlertDialogCancel>
+            <AlertDialogAction
+              className="bg-white text-red-600 border border-red-600 hover:bg-red-600 hover:text-white"
+              onClick={onClickDelete}
+            >
+              削除する
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
   );
 }
