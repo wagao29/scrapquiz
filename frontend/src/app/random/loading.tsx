2d557f365e833ec907fb9ebf6b27fb429671912d
wagao <wagao1029@gmail.com>
2024-09-24 19:16:31 +0900

    Fix tab select for random page loading

--- frontend/src/app/random/loading.tsx
+++ frontend/src/app/random/loading.tsx
@@ -5,7 +5,7 @@ import UpdateButton from "@/components/update-button";
 export default function Loading() {
   return (
     <main className="flex flex-col gap-4 items-center mx-4">
-      <TabMenu selectValue="latest" className="mb-4" />
+      <TabMenu selectValue="random" className="mb-4" />
       <SkeletonQuizCard />
       <SkeletonQuizCard />
       <SkeletonQuizCard />
