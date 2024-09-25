b701171c191ba9a1ffe1a6f64da4562d81d74d7e
wagao <wagao1029@gmail.com>
2024-09-25 23:13:05 +0900

    Add not found page

---
+++ frontend/src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="flex flex-col items-center justify-center">
+      <h1 className="text-2xl mt-64">404 Not Found</h1>
+      <p className="text-gray">Could not find the page</p>
+    </main>
+  );
+}
